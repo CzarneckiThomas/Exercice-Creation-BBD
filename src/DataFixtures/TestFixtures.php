ae1c9da75b4cb71e33459de47731dfad4fcd8155
ThomasCza <czarnecki.thomas@orange.fr>
2022-07-20 00:39:24 +0200

    Creation des DataFixtures
    ok

--- src/DataFixtures/TestFixtures.php
+++ src/DataFixtures/TestFixtures.php
@@ -33,20 +33,24 @@ class TestFixtures extends Fixture
     public function loadProjects(ObjectManager $manager, FakerGenerator $faker): void
     {
         $projectNames = [
-            'Site Promo',
-            'Site Association',
-            'Site entreprise',
+            'Site Particuliers',
+            'Site Associations',
+            'Site Entreprises',
         ];
 
         foreach ($projectNames as $projectName) {
             $project = new Project();
             $project->setName($projectName);
+            $project->setDescription($projectDescription);
+            
             $manager->persist($project);
         }
 
         for ($i = 0; $i < 10; $i++) {
             $project = new Project();
             $project->setName($faker->word());
+            $project->setDescription($faker->paragraph());
+
             $manager->persist($project);
         }
 
@@ -55,21 +59,31 @@ class TestFixtures extends Fixture
 
     public function loadSchoolYears(ObjectManager $manager, FakerGenerator $faker)
     {
-        $schoolYearsNames = [
+        $schoolYearNames = [
             '2022',
             '2023',
             '2024',
         ];
 
-        foreach ($schoolYearsNames as $schoolYearsName) {
-            $tag = new SchoolYear();
-            $tag->setName($schoolYearsName);
+        foreach ($schoolYearNames as $schoolYearName) {
+            $schoolYear = new SchoolYear();
+            $schoolYear->setName($schoolYearName);
+            $schoolYear->setStarted_at($schoolYearStart);
+            $schoolYear->setFinished_at($schoolYearEnd);
+
+            
             $manager->persist($schoolyear);
         }
 
         for ($i = 0; $i < 10; $i++) {
-            $tag = new SchoolYear();
-            $tag->setName($faker->numberBetween(2022, 2032));
+            $schoolYear = new SchoolYear();
+            $schoolYear->setName($faker->numberBetween(2022, 2032));
+
+            $date = $faker->dateTimeThisYear();
+            $date = DateTimeImmutable::createFromInterface($date);
+
+            $schoolYear->setStarted_at($date)
+            $schoolYear->setFinished_at($faker->
             $manager->persist($schoolYear);
         }
 
@@ -85,4 +99,46 @@ class TestFixtures extends Fixture
             $student->setLastname($studentLastName);
             $student->setEmail($studentEmail);
 
+            $manager->persist($student);
+
+        }
+
+        for (($i = 0; $i < 10; $i++)) {
+            $student = new SchoolYear();
+            $student->setFirstname($faker->name());
+            $student->setLastname($faker->name());
+            $student->setEmail($faker->email());
+
+            $manager->persist($student);
+
+        }
+        $manager->flush();
+    }
+
+    public function loadTags(ObjectManager $manager, FakerGenerator $faker): void
+    {
+        
+        foreach ($tags as $tag) {
+            $tag = new Tag();
+            $tag->setName($tagName);
             
+
+            $manager->persist($tag);
+
+        }
+
+        for (($i = 0; $i < 10; $i++)) {
+            $tag = new Tag();
+            $tag->setName($faker->word());
+            
+
+            $manager->persist($tag);
+
+        }
+        $manager->flush();
+    }
+
+    
+
+
+
